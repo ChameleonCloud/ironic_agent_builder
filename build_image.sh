c002c5149a9cdd18c0e760ff7b4fa31fbf33793b
Michael Sherman <shermanm@uchicago.edu>
2022-07-13 15:13:27 -0500

    initial commit

---
+++ build_image.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e -u -o pipefail
+
+# Set DIB elements path based on install location
+export ELEMENTS_PATH=.venv/share/ironic-python-agent-builder/dib
+
+# set references for stable/xena release, not master
+export DIB_REPOREF_ironic_python_agent=stable/xena
+export DIB_REPOREF_ironic_lib=stable/xena
+export DIB_REPOREF_requirements=stable/xena
+
+#10s timeout for dhcp
+#export DIB_DHCP_TIMEOUT=10
+
+#don't remove firmware
+export IPA_REMOVE_FIRMWARE=[]
+
+export ARCH=amd64
+export DIB_RELEASE=bullseye
+
+IMAGE_NAME="ipa-debian-${DIB_RELEASE}-${ARCH}-stable-xena"
+INITRAMFS_NAME="${IMAGE_NAME}.initramfs"
+KERNEL_NAME="${IMAGE_NAME}.kernel"
+
+source .venv/bin/activate
+
+disk-image-create \
+	-o "${IMAGE_NAME}" \
+	ironic-python-agent-ramdisk \
+	burn-in \
+	dynamic-login \
+	debian-minimal
