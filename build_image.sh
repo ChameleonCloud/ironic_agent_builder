294fd17e996378a2bace9e8408a4ffbb0672f7ef
Michael Sherman <shermanm@uchicago.edu>
2022-07-26 13:20:11 -0500

    customize build properties

--- build_image.sh
+++ build_image.sh
@@ -3,22 +3,24 @@
 set -e -u -o pipefail
 
 # Set DIB elements path based on install location
-export ELEMENTS_PATH=.venv/share/ironic-python-agent-builder/dib:/opt/ipa_build/dib
+export ELEMENTS_PATH=.venv/share/ironic-python-agent-builder/dib:./dib
 
+
+IRONIC_BRANCH=${IRONIC_BRANCH-stable/xena}
 # set references for stable/xena release, not master
-export DIB_REPOREF_ironic_python_agent=stable/xena
-export DIB_REPOREF_ironic_lib=stable/xena
-export DIB_REPOREF_requirements=stable/xena
+export DIB_REPOREF_ironic_python_agent=${IRONIC_BRANCH}
+export DIB_REPOREF_ironic_lib=${IRONIC_BRANCH}
+export DIB_REPOREF_requirements=${IRONIC_BRANCH}
 
 #10s timeout for dhcp
-# export DIB_DHCP_TIMEOUT=10
+export DIB_DHCP_TIMEOUT=10
 # disable module if networkmanager found
 export DIB_DHCP_NETWORK_MANAGER_AUTO=true
 
 #don't remove firmware
 # export IPA_REMOVE_FIRMWARE=[]
 
-export ARCH=amd64
+export ARCH=${ARCH-amd64}
 export DIB_RELEASE=bullseye
 
 IMAGE_NAME="ipa-debian-${DIB_RELEASE}-${ARCH}-stable-xena"
